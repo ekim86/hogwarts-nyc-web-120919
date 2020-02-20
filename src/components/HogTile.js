8af04c9a083d48b308f1aa569f3c886ced97b05d
github username <github email address>
2020-02-20 14:28:50 -0500

    click works

--- src/components/HogTile.js
+++ src/components/HogTile.js
@@ -1,14 +1,48 @@
+// import React from 'react';
+
+// class HogTile extends React.Component {
+  
+//   render(props){
+// console.log(this.props)
+//     return (
+//       <div className="ui eight wide column">
+//         <div>
+   
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+  
+//   export default HogTile;
+
 import React from 'react';
 
 class HogTile extends React.Component {
+
+  state = {
+    clickedOn: false
+  }
+
+  handlePigTile = () => {
+
+    let currentState = this.state.clickedOn
+    this.setState({
+      clickedOn: !currentState
+    })
+
+  }
   
   render(){
+    console.log(this.state.clickedOn, 'CLICK')
+    const {hogInfoz: {name, specialty, greased, weight}} = this.props;
+
     return (
-
-        <div>
-   
-        </div>
-
+      <div onClick={this.handlePigTile} className="pigTile" >
+        <h3>{name}</h3>
+        {(this.state.clickedOn) ? `${specialty} ${greased} ${weight}` : null}<br/>
+        {<img src={process.env.PUBLIC_URL + `/hog-imgs/${name.split(' ').join('_')}.jpg`} alt={name} />}
+      </div>
     );
   }
 }
--- src/components/HogsContainer.js
+++ src/components/HogsContainer.js
@@ -1,27 +1,72 @@
+// import React from 'react'
+// import hogsdata from '../porkers_data';
+
+// class HogsContainer extends React.Component{
+
+
+
+//   render(){
+//     // console.log('hogs', this.props);
+
+//     return(
+//       <div>
+//         {hogsdata.map((hog, index) =>
+//         <div className='pigTile'>
+//           {hog.index}
+//           {hog.name}
+//           {hog.specialty}
+//           {hog.greased}
+//           {hog.weight}
+//           <img src="../hog-imgs/cherub.jpg"/>
+//       </div>)}
+//       </div>
+//     )
+
+//   }
+// }
+
+// export default HogsContainer
+
 import React from 'react'
 import hogsdata from '../porkers_data';
+import { render } from 'react-dom';
+import HogTile from './HogTile'
 
-class HogsContainer extends React.Component{
+class HogsContainer extends React.Component {
+
+  // state = {
+  //   clickedOn: false
+  // }
+
+  // handlePigTile = () => {
+
+  //   let currentState = this.state.clickedOn
+  //   this.setState({
+  //     clickedOn: !currentState
+  //   })
+
+  // }
+
+  render() {
+    // console.log(this.state.clickedOn, 'CLICK')
+    let hogTileInfo = hogsdata.map((hogInfo, index) =>
+      <HogTile
+        key={index}
+        hogInfoz={hogInfo}
+        // clickedTest={this.state.clickedOn}
+        // {...hogInfo}
+      />)
 
 
-
-  render(){
-    // console.log('hogs', this.props);
-
-    return(
-      <div>
-        {hogsdata.map((hog, index) =>
-        <div className='pigTile'>
-          {hog.name}
-          {hog.specialty}
-          {hog.greased}
-          {hog.weight}
-          <img src="../hog-imgs/cherub.jpg"/>
-      </div>)}
+    return (
+      <div className="hog-container">
+        {hogTileInfo}
       </div>
-    )
+    );
 
   }
+
+
 }
 
 export default HogsContainer
